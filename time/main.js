3d4f61d06c667da51da5fa5dab6fd43e57d78ca2
konasek <kubajzik20@gmail.com>
2022-11-16 17:08:34 +0100

    Add files via upload

--- time/main.js
+++ time/main.js
@@ -1,23 +1,26 @@
 const odpocet = () => {
-    const datum = new Date("May 17, 2023 00:00:00").getTime
+    const datum = new Date("January 17, 2023 00:00:00")
     const now = new Date().getTime();
     const gap = datum - now;
 
+    console.log(datum)
+
     const vterina = 1000;
-    const minuta = vterina * 60
-    const hodina = minuta * 60
-    const den = hodina * 24
+    const minuta = vterina * 60;
+    const hodina = minuta * 60;
+    const den = hodina * 24;
 
-    const textden = math.floor(gap / den);
-    const texthodina = math.floor((gap % den) / hodina);
-    const textminuta = math.floor((gap % hodina) / minuta);
-    const textvterina = math.floor((gap % minuta) / vterina);
+    const textden = Math.floor(gap / den);
+    const texthodina = Math.floor((gap % den) / hodina);
+    const textminuta = Math.floor((gap % hodina) / minuta);
+    const textvterina = Math.floor((gap % minuta) / vterina);
 
-    console.log (now);
-
-    document.querySelector(".container-den").innerText = textden;
-
+    document.querySelector(".den").innerText = textden;
+    document.querySelector(".hodina").innerText = texthodina;
+    document.querySelector(".minuta").innerText = textminuta;
+    document.querySelector(".vterina").innerText = textvterina;
 
 };
 
-odpocet();
+
+setInterval(odpocet, 1000)
