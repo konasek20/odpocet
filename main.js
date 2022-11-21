7cfb6bd1aaee4b86c27da79b104718a9502fc2e2
konasek <kubajzik20@gmail.com>
2022-11-21 11:01:19 +0100

    verze 0.1

--- main.js
+++ main.js
@@ -2,19 +2,17 @@ const odpocet = () => {
     const datum = new Date("January 17, 2023 00:00:00")
     const now = new Date().getTime();
     const gap = datum - now;
-
-    console.log(datum)
-
+//definice času
     const vterina = 1000;
     const minuta = vterina * 60;
     const hodina = minuta * 60;
     const den = hodina * 24;
-
+//matika 
     const textden = Math.floor(gap / den);
     const texthodina = Math.floor((gap % den) / hodina);
     const textminuta = Math.floor((gap % hodina) / minuta);
     const textvterina = Math.floor((gap % minuta) / vterina);
-
+// výpis to HTML
     document.querySelector(".den").innerText = textden;
     document.querySelector(".hodina").innerText = texthodina;
     document.querySelector(".minuta").innerText = textminuta;
@@ -22,5 +20,5 @@ const odpocet = () => {
 
 };
 
-
+// zapnutí opkovací funkce 
 setInterval(odpocet, 1000)
